8705f10998a633af77530a0b516565309cfff988
datguyandres <spartansup18@gmail.com>
2025-06-26 14:50:49 -0700

    Array of checkpoints  + sorted properly

--- Assets/CheckPointSetup.cs
+++ Assets/CheckPointSetup.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
-//public GameObject[] CheckPoints;
 public class CheckPointSetup : MonoBehaviour
 {
+
+    public GameObject[] CheckPointArray;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //List<GameObject> CheckPointList;
     void Start()
     {
-        //CheckPoints = new GameObjecst[transform.childCount];
-
+        CheckPointArray = new GameObject[transform.childCount];
+        CheckPointArray = GameObject.FindGameObjectsWithTag("CheckPoint");
+        System.Array.Sort(CheckPointArray,
+                   (a, b) => { return a.transform.GetSiblingIndex().CompareTo(b.transform.GetSiblingIndex()); });
+        System.Array.Reverse(CheckPointArray);
+        
     }
 
     // Update is called once per frame
