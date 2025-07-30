475b427c7761be7dc9af64243d7e7914a5445766
falicov <121999608+falicov@users.noreply.github.com>
2025-07-29 17:20:07 -0700

    scene is fully functional (looks bad tho)

--- Assets/Scripts/GoToScene.cs
+++ Assets/Scripts/GoToScene.cs
@@ -4,6 +4,11 @@ using UnityEngine.SceneManagement;
 
 public class GoToScene : MonoBehaviour
 {
+    [Header("Optional")]
+    [SerializeField] private Collider2D collider2D;
+
+    [SerializeField] private string scene;
+
     /// <summary>
     /// Loads a given scene
     /// </summary>
@@ -11,5 +16,14 @@ public class GoToScene : MonoBehaviour
     public void LoadTheScene(string scene) 
     {
         SceneManager.LoadScene(scene); 
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            LoadTheScene(scene);
+        }
     }
 }
