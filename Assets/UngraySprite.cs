e9671dc2d65dd85aa10a13362418565769c83a49
blursified <serenacai24@gmail.com>
2025-08-05 18:02:50 -0700

    THE END SCREEN SPRITES ARE GRAYED OUT NOW FINALLY

---
+++ Assets/UngraySprite.cs
@@ -0,0 +1,24 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class UngraySprite : MonoBehaviour
+{
+    public int NPCNum;
+    public UnityEngine.UI.Image image; 
+
+ 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Instance.NpcsBeaten[NPCNum] == NPCNum)
+        {
+            image.color = new Color(255,255,255,255);
+        }
+    }
+}
